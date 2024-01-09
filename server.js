997dfcbe84883a54498ff83e4051ec1dd5a3d9ae
Kamal <kamal_dia@hotmail.com>
2024-01-09 16:54:25 +0200

    created server and folders

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+// import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to the database:", err);
+  });
