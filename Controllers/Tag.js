e1b9388b2f35ec4090397de5e5ff33fcbe5b2d04
zahraaAed <zahraaalaaeddine94@gmail.com>
2024-01-13 20:40:37 +0200

    editing tag controller by adding product to it

--- Controllers/Tag.js
+++ Controllers/Tag.js
@@ -1,5 +1,6 @@
 
 import Tag from "../Models/Tag.js";
+import Product from "../Models/Product.js";
 export const getAllTags = async (req, res) => {
     try {
       const tags = await Tag.find();
@@ -9,12 +10,10 @@ export const getAllTags = async (req, res) => {
       res.status(500).send('Internal Server Error');
     }
   };
-  
-
   export const getTagById=async (req, res) => {
-    const { id } = req.params;
+   
     try {
-      const tag = await Tag.findById(id);
+      const tag = await Tag.findById(req.params.id);
       if (!tag) {
         return res.status(404).json({ message: 'tag not found' });
       }
@@ -26,47 +25,76 @@ export const getAllTags = async (req, res) => {
   };
 
  export const createTag=async (req, res) => {
-    const { name} = req.body;
+  try {
+    const { products } = req.body; 
+    const new_tag = new Tag(req.body);
 
-    try {
-        
-      const newtag = await Tag.create({ name });
-      res.status(201).json(newtag);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
+    var found_all_products_flag = true;
+    var wrong_products_input = [];
+
+    for (let i = 0; i < products.length; i++) {
+      var product = await Product.findById(products[i]);
+
+      if (!product) {
+        wrong_products_input.push(products[i]);
+        found_all_products_flag = false;
+      }
     }
 
- };
+    if (!found_all_products_flag) {
+      return res.status(404).json({ error: `Products with ids ${wrong_products_input} not found` });
+    }
+
+    const createdTag = await new_tag.save();
+    res.status(200).json(createdTag);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
   export const updateTag=async (req, res) => {
-    const { id } = req.params;
-    const { name} = req.body;
-
     try {
-
-
-      const updatedtag = await Tag.findByIdAndUpdate(
-        id,
-        { name },
-       
+      const { products } = req.body; // Corrected: Use 'products' instead of 'Product'
+      const tag_update_copy = req.body;
+  
+     
+  
+      var found_all_products_flag = true;
+      var wrong_products_input = [];
+  
+      for (let i = 0; i < products.length; i++) {
+        var product = await Product.findById(products[i]);
+  
+        if (!product) {
+          wrong_products_input.push(products[i]);
+          found_all_products_flag = false;
+        }
+      }
+  
+      if (!found_all_products_flag) {
+        return res.status(404).json({ error: `Products with ids ${wrong_products_input} not found` });
+      }
+  
+      const updatedTag= await Tag.findByIdAndUpdate(
+        req.params.id,
+        tag_update_copy,
+        { new: true, runValidators: true }
       );
-
-      if (!updatedtag) {
+  
+      if (!updatedTag) {
         return res.status(404).json({ message: 'tag not found' });
       }
-
-      res.json(updatedtag);
+  
+      res.status(200).json(updatedTag);
     } catch (error) {
-      console.error(error.message);
-      res.status(500).send('Internal Server Error');
+      res.status(400).json({ error: error.message });
     }
   };
 
   export const deleteTag = async (req, res) => {
-    const { id } = req.params;
+    
   
     try {
-      const deletedtag = await Tag.findByIdAndDelete(id);
+      const deletedtag = await Tag.findByIdAndDelete(req.params.id);
   
       if (!deletedtag) {
         return res.status(404).json({ message: 'tag not found' });
